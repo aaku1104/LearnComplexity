e92108d1319ef47bbd4783917ea75a68f23bfb55
aaku1104 <jagadalesakshi01@gmail.com>
2026-03-27 10:56:51 +0530

    Added pages and layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadComponent: () => import('./pages/home/home').then(m => m.Home) },
+  { path: 'about', loadComponent: () => import('./pages/about/about').then(m => m.About) },
+  { path: 'contact', loadComponent: () => import('./pages/contact/contact').then(m => m.Contact) },
+  { path: 'courses', loadComponent: () => import('./pages/courses/courses').then(m => m.Courses) },
+];
